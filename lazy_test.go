8ef41b3b4cca682bf5288a84c73bc2c812b5dd5f
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-01 15:18:59 -0800

    (test) Add a test for the workload running just once.

--- lazy_test.go
+++ lazy_test.go
@@ -4,40 +4,66 @@ import "testing"
 import "sync"
 
 type Data struct {
-    x int
+	x int
 }
 
 func TestForce(t *testing.T) {
-    // It is only legal to access data after thunk.Force has been called.
-    data := &Data{0}
-    thunk := Defer(func() {
-        data.x = 45
-    })
+	// It is only legal to access data after thunk.Force has been called.
+	data := &Data{0}
+	thunk := Defer(func() {
+		data.x = 45
+	})
 
-    // "Expensive computation run!" will be printed once
-    // some time after this.
+	// "Expensive computation run!" will be printed once
+	// some time after this.
 
-    var wg sync.WaitGroup
-    wg.Add(2)
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		defer wg.Done()
 
-        thunk.Force()
-        if data.x != 45 {
-            t.Fatal("data.x has an unexpected value of:", data.x)
-        }
-    }()
+		thunk.Force()
+		if data.x != 45 {
+			t.Fatal("data.x has an unexpected value of:", data.x)
+		}
+	}()
 
-    go func() {
-        defer wg.Done()
+	go func() {
+		defer wg.Done()
 
-        thunk.Force()
-        if data.x != 45 {
-            t.Fatal("data.x has an unexpected value of:", data.x)
-        }
-    }()
+		thunk.Force()
+		if data.x != 45 {
+			t.Fatal("data.x has an unexpected value of:", data.x)
+		}
+	}()
 
-    wg.Wait()
+	wg.Wait()
 }
 
+func TestRunsWorkOnec(t *testing.T) {
+	counter := 0
+	thunk := Defer(func() {
+		counter += 1
+	})
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for range make([]struct{}, 10) {
+		go func() {
+			defer wg.Done()
+
+			thunk.Force()
+			thunk.Force()
+			thunk.Force()
+			thunk.Force()
+			thunk.Force()
+		}()
+	}
+
+	wg.Wait()
+	if counter != 1 {
+		t.Fatal("The lazy workload was run more than once.")
+	}
+}
