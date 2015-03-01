3bfd43c4081d25e1445cf9d81130e65395e2f753
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-01 15:37:27 -0800

    (doc) Add some documentation for methods and types.

--- lazy.go
+++ lazy.go
@@ -8,16 +8,23 @@ const (
 	complete   uint32 = 2
 )
 
+// A type which lazily evaluates a workload, represented
+// as a 0-argument function.
+//
+// Constructed using Defer
 type Lazy struct {
 	state uint32
 	mu    *oncemutex.OnceMutex
 	work  func()
 }
 
+// Create a new instance of Lazy which manages a func() workload.
 func Defer(work func()) *Lazy {
 	return &Lazy{start, oncemutex.NewOnceMutex(), work}
 }
 
+// Force an existing Lazy, this blocks until the workload has been
+// finished at least once.
 func (l *Lazy) Force() {
 	defer l.mu.Unlock()
 
